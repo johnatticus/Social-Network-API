0d5a0a74099cb0834e49ac46466ffa3bfd31aa0a
John Hysong <johnahysong@gmail.com>
2022-05-19 21:29:48 -0400

    worked on controllers, began dateFormat

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    //CRUD COMMANDS
+    // create a user
+    // get a user by id
+    // update a user
+    // delete a user
+}
+
+module.exports = userController;
