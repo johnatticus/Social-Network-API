27422677ffca1723d0738a628398773e1f4b6ccf
John Hysong <johnahysong@gmail.com>
2022-05-21 20:34:36 -0400

    updated userController for update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,10 +74,23 @@ const userController = {
         });
     },
 
-    // Update a user
+    // Update a user...WORKS
     updateUser(req, res) {
-
-    },
+        User.findOneAndUpdate(
+          { _id: req.params.userId },
+          { $set: req.body },
+          { runValidators: true, new: true }
+        )
+          .then((user) =>
+            !user
+              ? res.status(404).json({ message: 'No user with this id!' })
+              : res.json(user)
+          )
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
 
     // Add a friend...WORKS
     addFriend(req, res) {
